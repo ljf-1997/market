42daddf8561390880e2bcc7687354fe5f3ee7030
ljf <1285800895@qq.com>
2022-05-13 16:29:34 +0800

    111

--- config/index.js
+++ config/index.js
@@ -10,7 +10,14 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+      target: 'http://localhost:8443',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/'
+      }
+    }},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -72,14 +79,5 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
-  },
-  proxyTable: {
-    '/api': {
-      target: 'http://localhost:8443',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': ''
-      }
-    }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
+import AppIndex from '../components/home/AppIndex'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import WaresIndex from '../components/wares/WaresIndex'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/wares',
+          name: 'Wares',
+          component: WaresIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
     },
     {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex
     }
   ]
 })
+
