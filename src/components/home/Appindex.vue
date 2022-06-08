25e6862ae9ad9be1d0396c317086902749543df2
ljf <1285800895@qq.com>
2022-06-08 11:10:36 +0800

    更新

--- src/components/home/Appindex.vue
+++ src/components/home/Appindex.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-carousel :interval="5000" arrow="always">
+  <el-carousel indicator-position="outside">
     <el-carousel-item v-for="item in 4" :key="item">
       <h3>{{ item }}</h3>
     </el-carousel-item>
